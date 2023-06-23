334b4d807151ac2862d92493db7bd56256a42d90
James <jamesmcdaniel33@hotmail.com>
2023-06-23 11:43:36 -0500

    insert product Ex4

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -45,6 +45,18 @@ namespace Testing.Controllers
 
             return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Models.Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        }
     }
 }
 
--- Testing/ProductRepo.cs
+++ Testing/ProductRepo.cs
@@ -22,12 +22,32 @@ namespace Testing
 
         public Product GetProduct(int id)
         {
-            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id", new { @id = id });
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id",
+                new { @id = id });
         }
 
         public void UpdateProduct(Product product)
         {
-            _conn.Execute("UPDATE products SET Name = @name, Price = @price, WHERE ProductID = @id", new { @name = product.Name, @price = product.Price, @id = product.ProductID });
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { @name = product.Name, @price = product.Price, @id = product.ProductID });
+        }
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+                new { @name = productToInsert.Name, @price = productToInsert.Price, @categoryID = productToInsert.CategoryID });
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
         }
     }
 }
